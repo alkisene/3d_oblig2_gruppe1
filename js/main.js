b5daf7e2965d2c0d06239643e431bbd40325ffcb
Edvard Vindenes Steenslid <142335301+h676669@users.noreply.github.com>
2025-11-17 15:06:58 +0100

    import ting

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 "use strict";
+// kjør "npx vite" for å kjøre programmet.
+
+// hentet eksempel kode fra : https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_terrain_raycast.html
+
+import * as THREE from "three";
+
+import { OrbitControls} from "three/addons";
+
+let camera, controls, scene, renderer;
+
+let worldMesh, worldTexture;
+
+
 
